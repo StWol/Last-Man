c1c27e5ecb688df46374fd1c5b7d97e4ed731ba1
Stanislaw <sw128@hdm-stuttgart.de>
2012-05-14 18:28:34 +0200

    Renderer Klassen verschoben

---
+++ EVCS-Projekt/EVCS-Projekt/Renderer/IRenderBehavior.cs
@@ -0,0 +1,13 @@
+﻿using EVCS_Projekt.Location;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace EVCS_Projekt.Renderer
+{
+    interface IRenderBehavior
+    {
+        void Draw(SpriteBatch spriteBatch, ILocationBehavior locationBehavoir);
+        void Update(GameTime gameTime);
+
+    }
+}
