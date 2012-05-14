7a70be7089a10b1e3d0946400924562de0212d0b
JanHo <jh094@hdm-stuttgart.de>
2012-05-14 16:53:38 +0200

    IRenderBehavior.cs added | ReadMe modified

---
+++ EVCS-Projekt/EVCS-Projekt/IRenderBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace EVCS_Projekt
+{
+    interface IRenderBehavior
+    {
+        public void draw(SpriteBatch sriteBatch, ILocationBehavoir locationBehavior);
+        public void Update(GameTime gameTime);
+    }
+}
