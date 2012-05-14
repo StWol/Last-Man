ac3aa32cd5ab238cf73a4145b68edadc29c425d8
JanHo <jh094@hdm-stuttgart.de>
2012-05-14 16:47:53 +0200

    UILocation.cs added

---
+++ EVCS-Projekt/EVCS-Projekt/UILocation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace EVCS_Projekt
+{
+    class UILocation : ILocationBehavoir
+    {
+
+        private Rectangle rectangle;
+        private Vector2 size;
+        private Vector2 position;
+        public Rectangle _Rechtangle
+        {
+            get
+            {
+                return this.rectangle;
+            }
+        }
+
+        public UILocation(Vector2 position, Vector2 size)
+        {
+            this.position.X = rectangle.X;
+            this.position.Y = rectangle.Y;
+            this.size.X = rectangle.Width;
+            this.size.Y = rectangle.Height;
+
+        }
+        public UILocation(Rectangle rectangle)
+        {
+
+        }
+
+        public Vector2 GetPostition()
+        {
+            Vector2 tmp;
+            tmp.X = rectangle.X;
+            tmp.Y = rectangle.Y;
+            return tmp;
+        }
+
+        public Vector2 GetSize()
+        {
+            Vector2 tmp;
+            tmp.X = rectangle.Width;
+            tmp.Y = rectangle.Height;
+            return tmp;
+        }
+    }
+}
