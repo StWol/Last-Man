b6c35283bc49420b1701251e0f1102c3bfaf9b27
Stan <sw128@hdm-stuttgart.de>
2012-06-19 21:09:59 +0200

    UIToggleButton vergessen :)

---
+++ EVCS-Projekt/EVCS-Projekt/UI/UIToggleButton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace EVCS_Projekt.UI
+{
+    class UIToggleButton: UIButton
+    {
+        private Texture2D activeTexture;
+        private bool isActive;
+
+        public UIToggleButton(int width, int height, Vector2 position, Texture2D texture, Texture2D hoverTexture, Texture2D activeTexture, string text) 
+            : base(width, height, position, texture, hoverTexture, text)
+        {
+            this.activeTexture = activeTexture;
+            isActive = false;
+        }
+
+        public override void Draw(SpriteBatch sb)
+        {
+            if (IsMousePressed() && isActive)
+            {
+                List<UIActionListener> listenerList = new List<UIActionListener>(listener);
+                foreach (UIActionListener al in listenerList)
+                {
+                    al.OnMouseUp(this);
+                }
+            }
+            else
+            {
+                //texture = activeTexture;
+            }
+            base.Draw(sb);
+        }
+    }
+}
